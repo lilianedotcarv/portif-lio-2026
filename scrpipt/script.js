16e78ec72d1ee7a36254426bf1087abd068b771f
lilianedotcarv <menezesliliane16@gmail.com>
2026-06-05 18:50:43 -0300

    Add files via upload

---
+++ scrpipt/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.carousel-img');
+    let currentIndex = 0;
+
+    if (images.length > 0) {
+        setInterval(() => {
+            images[currentIndex].classList.remove('active');
+            currentIndex = (currentIndex + 1) % images.length;
+            images[currentIndex].classList.add('active');
+        }, 3000); 
+    }})
